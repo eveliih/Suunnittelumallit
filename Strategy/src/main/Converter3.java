f0ab19227b7cc86f50593d9aebc4d4f61835694f
Eveliina <eveliina.heino@metropolia.fi>
2023-03-01 21:15:24 +0200

    Strategy

--- Strategy/src/main/Converter3.java
+++ Strategy/src/main/Converter3.java
@@ -8,12 +8,14 @@ public class Converter3 implements ListConverter {
 	public String listToString(List<Integer> lista) {
 		Integer[] taulukko = new Integer[lista.size()];
 		lista.toArray(taulukko);
+		
 		String palautettava = "";
+		
 		for (int i = 0; i < taulukko.length; i++) {
 			if ((i + 1) % 3 == 0) {
-				palautettava += lista.get(i) + "\n";
+				palautettava += taulukko[i] + "\n";
 			} else {
-				palautettava += lista.get(i);
+				palautettava += taulukko[i];
 			}
 		}
 		palautettava += "\n";
